4d3a8bdaa887c0864662f69849a3d64baa5d6624
AshCherr96 <ashleycherry16@gmail.com>
2026-07-05 15:29:15 -0500

    Add core functionality and stream demo for Node core modules assignment

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,57 @@
 const fs = require('fs');
 const path = require('path');
 
+const dir = path.join(__dirname, 'sample-files');
+const filePath = path.join(dir, 'sample.txt');
+const content = 'Hello, async world!';
 
-// Write a sample file for demonstration
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+fs.writeFileSync(filePath, content);
 
-// 1. Callback style
+/**
+ *  - Callback Pattern
+ * Callback hell occurs when you nest multiple asynchronous operations,
+ * making code hard to read and maintain (the "pyramid of doom").
+ * 
+ * Example of callback hell:
+ * fs.readFile(file1, (err, data) => {
+ *   fs.readFile(file2, (err, data) => {
+ *     fs.readFile(file3, (err, data) => {
+ *       // ... more nesting
+ *     });
+ *   });
+ * });
+ */
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) return console.error(err);
+    console.log('Callback read:', data);
 
+    // Promise Pattern
+    const readFilePromise = (path) => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, 'utf8', (err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+            });
+        });
+    };
 
-  // Callback hell example (test and leave it in comments):
+    readFilePromise(filePath)
+        .then((data) => {
+            console.log('Promise read:', data);
 
-
-  // 2. Promise style
-
-
-      // 3. Async/Await style
+            // Async/Await Pattern
+            async function runAsyncAwait() {
+                try {
+                    const data = await readFilePromise(filePath);
+                    console.log('Async/Await read:', data);
+                } catch (err) {
+                    console.error('Error:', err.message);
+                }
+            }
+            runAsyncAwait();
+        })
+        .catch((err) => console.error(err));
+});
